7f9dc68f2f10a94c205d22f266df9999306134e4
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-04-15 10:43:02 +0200

    chore(flake): add process compose and services-flake inputs

--- flake.nix
+++ flake.nix
@@ -7,6 +7,8 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
   };
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; }
